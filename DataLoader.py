6b69031164aefe8da78adfb982c083f0905844d3
Prasun Banerjee <prasunbanerjee@Prasuns-MBP.attlocal.net>
2025-01-31 10:59:35 -0500

    data loader 2

--- DataLoader.py
+++ DataLoader.py
@@ -1,1 +1,33 @@
+import pandas as pd
 
+class DataLoader:
+
+    def __init__(self, data):
+        self.data = data
+    
+    @classmethod
+    def extract_data(data):
+        #take api data stream and convert to dataframe
+
+
+        return data
+    
+    @classmethod
+    def transform_data(data):
+        #take two column data and integrate advanced features
+
+        
+        return data
+    
+    @classmethod
+    def load_data_to_csv(data):
+        #take pandas dataframe --> csv file 
+
+        
+        return data
+    
+    def load_data_to_parquet(data):
+        #take pandas dataframe --> parquet file 
+
+        
+        return data
